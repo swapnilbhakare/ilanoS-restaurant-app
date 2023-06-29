2e0a7897792ec342d24ed6ecc5282db517b4ac4c
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-06-29 22:03:41 +0530

    restarant summary added

---
+++ src/Components/Meals/Meals.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import stylesheet from "./Meals.module.css";
+import MealSummary from "./MealSummary";
+const Meals = () => {
+  return (
+    <>
+      <MealSummary />
+    </>
+  );
+};
+
+export default Meals;
